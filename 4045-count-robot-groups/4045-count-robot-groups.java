f0df97efbb6fae4c44c60817026cb153442993df
Tanishq Kathar <codelearner7171@gmail.com>
2026-09-08 23:46:10 +0530

    Time: 2 ms (100%), Space: 100 MB (56.14%) - LeetHub

---
+++ 4045-count-robot-groups/4045-count-robot-groups.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countGroups (int[] position, int[] speed, int distance) {
+        int n = position.length;
+        int ans = n ;
+        int rightMost = speed[n-1];
+
+        for(int i = n-2 ; i >= 0 ; i--){
+            if(position[i+1] - position[i] <= distance || rightMost < speed[i]){
+                ans--;
+            }
+            else{
+                rightMost = speed[i];
+            }
+        }
+
+        return ans ;
+    }
+}
