c7d39ffab0ab9824b8641a1e99de3410c5bc089f
Tanishq Kathar <codelearner7171@gmail.com>
2026-09-09 09:34:56 +0530

    Time: 1 ms (93.88%), Space: 43.7 MB (9.19%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -0,0 +1,63 @@
+
+class Solution { 
+
+    public boolean solve(String s1, String s2, String s3, int i, int j , Boolean[][] dp) { 
+
+        int k = i+j;
+        //base case
+        if(k == s3.length()){ 
+            return true; 
+        } 
+
+        if(dp[i][j] != null){
+            return dp[i][j];
+        }
+
+        boolean res = false; 
+
+        // both matching
+        if(i < s1.length() && j < s2.length() &&
+           s1.charAt(i) == s3.charAt(k) && 
+           s2.charAt(j) == s3.charAt(k)) { 
+
+            res = solve(s1, s2, s3, i+1, j , dp) 
+               || solve(s1, s2, s3, i, j+1 , dp); 
+        } 
+
+        // only s1 matching
+        else if(i < s1.length() &&
+                s1.charAt(i) == s3.charAt(k) && 
+                (j >= s2.length() || s2.charAt(j) != s3.charAt(k))) { 
+
+            res = solve(s1, s2, s3, i+1, j , dp); 
+        } 
+
+        // only s2 matching
+        else if(j < s2.length() &&
+                (i >= s1.length() || s1.charAt(i) != s3.charAt(k)) && 
+                s2.charAt(j) == s3.charAt(k)) { 
+
+            res = solve(s1, s2, s3, i, j+1 , dp); 
+        } 
+
+        return dp[i][j] =  res; 
+    } 
+
+    public boolean isInterleave(String s1, String s2, String s3) { 
+
+        int a = s1.length(); 
+        int b = s2.length(); 
+        int c = s3.length(); 
+
+       
+        if(a + b != c){
+            return false;
+        }
+
+        Boolean[][] dp = new Boolean[a+1][b+1];
+        
+
+        return solve(s1, s2, s3, 0, 0 , dp); 
+    } 
+}
+
